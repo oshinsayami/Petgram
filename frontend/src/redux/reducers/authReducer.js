1beb5d74407005b874d8cb9198bc3be3c23c3284
Simran <mdrsimran@gmail.com>
2021-03-16 01:58:40 -0500

    added a success case

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -1,5 +1,7 @@
 export default (state={loggedIn: false, currentUser: {}}, action) => {
     switch (action.type) {
+        case 'AUTH_SUCCESS':
+            return {...state, loggedIn: action.payload.loggedIn, currentUser: action.payload.currentUser}
         default:
             return state;
     }
