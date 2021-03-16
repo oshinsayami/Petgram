d84b7fd8626d771782f083145e4ebdb5af5e38b3
Simran <mdrsimran@gmail.com>
2021-03-16 11:42:30 -0500

    formatting changes

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,13 +1,19 @@
-export const signup = (user) => {
+export const signup = (user, history) => {
     return (dispatch) => {
         fetch(`http://localhost:3000/users`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ user: user})
+            body: JSON.stringify({ user: user })
         })
             .then(res => res.json())
-            .then(data => dispatch({type: 'AUTH_SUCCESS', payload: {loggedIn: data.logged_in, currentUser: data.user}}))
+            .then((data) => {
+                dispatch({
+                    type: 'AUTH_SUCCESS',
+                    payload: { loggedIn: data.logged_in, currentUser: data.user },
+                })
+                history.push(`/posts`)
+            })
     }
 }
