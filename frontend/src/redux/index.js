f0d9ca3d4e9967b48abd437cd40ddaf457c2242f
Simran <mdrsimran@gmail.com>
2021-03-16 02:30:21 -0500

    added thunk

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,10 +1,15 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import authReducer from './reducers/authReducer'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     auth: authReducer,
 })
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
