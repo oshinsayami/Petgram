bf98573d9631c613cde8df5dca5f736bc7168005
Simran <mdrsimran@gmail.com>
2021-03-15 23:56:42 -0500

    created users controller with create function

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            render json: {status: 201, user: user, logged_in: true} 
+        else
+            render json: {status: 500, message: 'There was an error in creating an account'}
+        end
+    end
+
+    private
+    
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
