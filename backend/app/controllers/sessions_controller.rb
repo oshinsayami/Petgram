0fe1fb8c18df575751a2d1fb250778156780ce94
Simran <mdrsimran@gmail.com>
2021-03-16 12:51:08 -0500

    fixed minor mistake

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
 
-    def create= User.find_by(email: params[:user][:password])
+    def create
+        user = User.find_by(email: params[:user][:email])
 
         if user && user.authenticate(params[:user][:password])
             session[:id] = user.id
-            render json: {status: 201, user: user, logged_in: true} 
+            render json: { status: 201, user: user, logged_in: true}
         else
-            render json: {status: 401, message: 'User not found or password incorrect'}
+            render json: { status: 401, message: 'User not found or password incorrect'}
         end
     end
 
