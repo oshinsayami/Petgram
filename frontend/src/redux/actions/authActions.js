154d48552ce2330901a8f847a9165cbef6c9f448
Simran <mdrsimran@gmail.com>
2021-03-16 12:52:18 -0500

    added auth for login

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -17,3 +17,23 @@ export const signup = (user, history) => {
             })
     }
 }
+
+export const login = (user, history) => {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/sessions`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ user: user })
+        })
+            .then(res => res.json())
+            .then((data) => {
+                dispatch({
+                    type: 'AUTH_SUCCESS',
+                    payload: { loggedIn: data.logged_in, currentUser: data.user },
+                })
+                history.push(`/posts`)
+            })
+    }
+}
