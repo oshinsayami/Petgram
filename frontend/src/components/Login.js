101f9d26a4fbfff81db0012980ddaa7d515a3e90
Simran <mdrsimran@gmail.com>
2021-03-16 11:51:35 -0500

    created a login class

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { login } from '../redux/actions/authActions'
+import {connect} from 'react-redux'
+
+class Login extends React.Component {
+    
+    state = {
+        email: '',
+        username: '',
+        password: '',
+    }
+
+    handleOnChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault()
+        this.props.login(this.state, this.props.history)
+    }
+
+    render(){
+        return (
+            <div>
+                <form onSubmit={this.handleOnSubmit}>
+                    <label>Email</label>
+                    <input name="email" value={this.state.email} onChange={this.handleOnChange} type='text' /><br />
+                    <label>Username</label>
+                    <input name="username" value={this.state.username} onChange={this.handleOnChange} type='text' /><br />
+                    <label>Password</label>
+                    <input name="password" value={this.state.password} onChange={this.handleOnChange} type='password' /><br />
+                    <button type='submit'>Log in</button>
+                </form>
+            </div>
+        )   
+    }
+    
+}
+
+export default connect(null, {login})(Login)
+
