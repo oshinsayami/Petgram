f3b259d3d6f797134d57c7279f5590d5e936876f
Simran <mdrsimran@gmail.com>
2021-03-16 01:07:04 -0500

    added route path for home, post and signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
 import Post from './components/Post'
+import Signup from './components/Signup'
+
+
+
 
 function App() {
 
@@ -8,7 +14,15 @@ function App() {
       <div className="app_header">
         <h1>Petgram</h1>
       </div>
-      <Post/>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/posts' component={Post} />
+          <Route exact path='/signup' component={Signup} />
+        </Switch>
+      </Router>
+      
+      
     </div>
   );
 }
