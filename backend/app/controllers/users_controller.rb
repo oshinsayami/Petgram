55c98ac57fac9ac7380a5929c21230dfb0761a49
Simran <mdrsimran@gmail.com>
2021-03-16 02:32:31 -0500

    added username in params

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
+        params.require(:user).permit(:email, :username, :password, :password_confirmation)
     end
 end
