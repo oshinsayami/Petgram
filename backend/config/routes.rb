8e8702dba03ce2d0f4b54559ff9bceab53d4c994
Simran <mdrsimran@gmail.com>
2021-03-17 12:20:27 -0500

    created routes from resource generator

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :likes
+  resources :comments
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources :sessions, only: [:create]
  resources :users, only: [:create]
