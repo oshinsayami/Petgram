3cef1d9437bc1d875e60ffef0fc85374861ee6f9
Simran <mdrsimran@gmail.com>
2021-03-16 20:14:40 -0500

    added a symbol

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    helper_method :logged_in, :current_user
+    helper_method :logged_in?, :current_user
 
-    def logged_in
+    def logged_in?
         !!current_user
     end
 
