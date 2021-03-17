a904a901bba1cc0ddef56b2b7231e9b1826d21d0
Simran <mdrsimran@gmail.com>
2021-03-17 18:11:07 -0500

    created methods for post controller

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: posts
+    end
+
+    def show 
+        post = Post.find_by(id: (paarams[:id])
+    end
+
+    def create
+        post = Post.new(post_params)
+        post.user_id = session[:user_id]
+        
+        if post.save
+            render :show
+        else
+            render ({ json: @post.errors.full_messages, status: 422 })
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:caption, :image)
+    end
 end
