6b0f7c90d5043dabcf3017b94d46cfa64c392681
Simran <mdrsimran@gmail.com>
2021-03-16 22:42:31 -0500

    added logout route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  resources :sessions, only: [:create]
  resources :users, only: [:create]
- get '/logged_in', to: 'sessions#logged_in'
+ get 'logged_in', to: 'sessions#logged_in'
+ delete 'logout', to: 'sessions#logout'
 end
