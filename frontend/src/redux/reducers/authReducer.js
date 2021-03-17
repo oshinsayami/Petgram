4962d19aa9243cc7c770f68db4d7ee421962e548
Simran <mdrsimran@gmail.com>
2021-03-16 22:46:31 -0500

    added logout case

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -2,6 +2,12 @@ export default (state={loggedIn: false, currentUser: {}}, action) => {
     switch (action.type) {
         case 'AUTH_SUCCESS':
             return {...state, loggedIn: action.payload.loggedIn, currentUser: action.payload.currentUser}
+        case 'LOGOUT':
+            return {
+                ...state,
+                loggedIn: false,
+                currentUser: {},
+            }
         default:
             return state;
     }
