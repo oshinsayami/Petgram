074b5cc2adbb3faefeacae0e7617680bd0d8d2c1
Simran <mdrsimran@gmail.com>
2021-03-18 00:01:49 -0500

    added seed

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+jack= User.create(username: "jack11", email: "jack@gmail.com", password:"password")
+
+post1= Post.create(caption:"The view", user_id: jack.id, image:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg")
