71d3224291e914448d0890c14adcfaa6f7a4900e
Simran <mdrsimran@gmail.com>
2021-03-19 10:53:50 -0500

    mapped the posts and rendered on page with correct post attributes

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+
 
 const PostList= ({posts}) => {
     return (
         <div>
-            {posts.map(post => <ul><li key="post.id">{post.image} - {post.caption}</li></ul>)}
+            {console.log(posts)}
+            {posts.map(post =>
+                <>
+                    <div className="post_header">
+                        {/* <Avatar className="post_avatar" /> */}
+                        <h3>{post.attributes.user.username}</h3>
+                    </div>
+                    <img className="post_image" src={post.attributes.image} />
+                    <h4 className="post_text"><strong>{post.attributes.user.username}</strong>{post.attributes.caption}</h4>
+                </>
+                )}   
         </div>
     )
 }
 
+ 
+
 const mapStateToProps = state => {
-    return { posts : state.posts }
+    // debugger
+    console.log(state)
+    return {
+        posts: state.posts,
+    }
 }
 
 export default connect(mapStateToProps)(PostList)
