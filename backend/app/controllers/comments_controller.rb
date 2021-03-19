75ef610226082046c4a9a51e1ac14258cea2d212
Simran <mdrsimran@gmail.com>
2021-03-19 10:55:50 -0500

    added comment controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+    def create
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            post = Post.find_by(id: comment.post_id)
+            render json: PostSerializer.new(post)
+        else
+            payload = {
+                error: 'Something went wrong. Please try again'
+                status: 400
+            }
+            render json: payload, status:bad_request
+        end
+    end
+
+
+    def comment_params
+        params.require(:comment).permit(:id, :body, :post_id, :user_id)
+    end
 end
