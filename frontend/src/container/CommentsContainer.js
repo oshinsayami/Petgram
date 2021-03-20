a0465cd6f47cc9af53120978c73ea22e8ae08fa2
Simran <mdrsimran@gmail.com>
2021-03-19 23:48:39 -0500

    made comments container

---
+++ frontend/src/container/CommentsContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import CommentInput from '../components/comments/CommentInput'
+import Comments from '../components/comments/Comments'
+import { connect } from 'react-redux'
+
+class CommentsContainer extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div>
+                <CommentInput addComment={this.props.addComment} postId={this.props.post.id} />
+                <Comments comments={this.props.post.comments} postId={this.props.post.id} deleteComment={this.props.deleteComment} />
+            </div>
+        )
+    }
+}
+
+export default CommentsContainer
