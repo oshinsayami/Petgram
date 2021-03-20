eb415423e21346f03a55baac15b224d6bf54e407
Simran <mdrsimran@gmail.com>
2021-03-19 23:49:12 -0500

    changed payload data to just the argument

--- frontend/src/redux/actions/postsAction.js
+++ frontend/src/redux/actions/postsAction.js
@@ -2,7 +2,7 @@ export const fetchPosts = () => {
     return (dispatch) => {
         fetch(`http://localhost:3000/posts`)
             .then(resp => resp.json())
-            .then(posts=> dispatch({type: 'FETCH_POSTS', payload: posts.data}))
+            .then(posts=> dispatch({type: 'FETCH_POSTS', payload: posts}))
     }
 }
 
@@ -16,9 +16,41 @@ export const addPost = (post, history) => {
             .then(resp => resp.json())
             .then(post => {
                 console.log(post)
-                dispatch({ type: 'ADD_POST', payload: post.data })
+                dispatch({ type: 'ADD_POST', payload: post})
                 history.push(`/posts`)
             })
     
     }
 }
+
+export const addComment = (comment) => {
+    return (dispatch) => {
+        fetch(`http://localhost:3000/comments`, {
+            method: 'POST',
+            body: JSON.stringify({comment: comment}),
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then(resp => resp.json())
+            .then(comment => {
+                console.log(comment)
+                dispatch({ type: 'ADD_COMMENT', payload: comment })
+            })
+    
+    }
+}
+
+// export const deleteComment = (comment) => {
+//     return (dispatch) => {
+//         fetch(`http://localhost:3000/comments`, {
+//             method: 'DELETE',
+//             body: JSON.stringify({comment: comment}),
+//             headers: { 'Content-Type': 'application/json' }
+//         })
+//             .then(resp => resp.json())
+//             .then(comment => {
+//                 console.log(comment)
+//                 dispatch({ type: 'ADD_COMMENT', payload: comment.data })
+//             })
+    
+//     }
+// }
