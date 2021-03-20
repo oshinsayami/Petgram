bd953472ec30cd97c2ca41834b4753067c3baea0
Simran <mdrsimran@gmail.com>
2021-03-19 23:48:24 -0500

    made Postdisplay for rendering single post

---
+++ frontend/src/components/posts/PostDisplay.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import CommentsContainer from '../../container/CommentsContainer'
+import {Avatar} from '@material-ui/core'
+
+class PostDisplay extends React.Component{
+    render() {
+        const { post } = this.props
+        console.log(post)
+        return (
+            // <div></div>
+            <div>
+                <div className="post_header">
+                    <Avatar className="post_avatar" />
+                    <h3>{post.user.username}</h3>
+                </div>
+                <img className="post_image" src={post.image} />
+                <h4 className="post_text"><strong>{post.user.username}</strong>{post.caption}</h4>
+                <CommentsContainer post={post} />
+            </div>
+    )
+    }
+    
+}
+
+export default PostDisplay
+
+
+
+
